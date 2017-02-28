a4bdef2ad17185cd9fc66f3ccc5825a956e8b04f
nikitparajuli <nikitparajuli@yahoo.com>
2017-02-28 15:16:12 -0800

    Angular 2 Basic App

---
+++ app/components/user.component.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+import { PostsService } from '../services/post.services';
+
+@Component({
+	moduleId: module.id,
+  	selector: 'user',
+  	templateUrl: 'user.component.html',
+  	providers: [PostsService]
+})
+export class UserComponent  { 
+	name: string;
+	email: string;
+	address: address;
+	hobbies: string[];
+	showHobbies: boolean;
+	posts: Post[];
+
+	constructor(private postsService: PostsService){
+		this.name = 'Nikit Parajuli';
+		this.email = "john@gmail.com";
+		this.address = {
+			street: "12 Main St",
+			city: "Boston",
+			state: "MA"
+		};
+		this.hobbies = ['Music', 'Movies', 'Sports'];
+		this.showHobbies = false;
+
+		this.postsService.getPosts().subscribe(posts => {
+				this.posts = posts;
+			});
+	}
+
+	toggleHobbies(){
+		this.showHobbies = !this.showHobbies;
+	}
+
+	addHobby(hobby: string){
+		this.hobbies.push(hobby);
+	}
+
+	deleteHobby(i: number){
+		this.hobbies.splice(i, 1);
+	}
+}
+
+interface address{
+	street: string;
+	city: string;
+	state: string;
+}
+
+interface Post{
+	id: number;
+	title: string;
+	body: string;
+}
